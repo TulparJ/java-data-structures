ca4110a83587e2e17fb98f6cf5dd6ee31c73212f
Tulpar Janbolat <tulparjanbolat.1@gmail.com>
2025-12-02 18:33:26 -0500

    Add files via upload

---
+++ OOP-FirmProject/Dog.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(name + " says: Woof! 🐶");
+    }
+}
